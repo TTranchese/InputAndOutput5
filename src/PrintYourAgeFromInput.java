c9cd823e9c1ef87888a5ec5e5f912ef2d53000cb
TTranchese <tommaso.tranchese.96@gmail.com>
2023-01-27 12:28:44 +0100

    initial commit

---
+++ src/PrintYourAgeFromInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PrintYourAgeFromInput {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your age:\s");
+		int age = input.nextInt();
+		System.out.printf("Your age is: %d", age);
+		input.close();
+
+	}
+}
